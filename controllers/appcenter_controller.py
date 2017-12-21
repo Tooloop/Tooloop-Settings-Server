6f388799c16243d771da5169566a85aa172f3367
vollstock <daniel@vollstock.de>
2017-12-21 15:44:46 +0100

    chowning the installed_app dir had wrong path

--- controllers/appcenter_controller.py
+++ controllers/appcenter_controller.py
@@ -185,7 +185,7 @@ class AppCenter(object):
             copytree(self.app_path+app+'/presentation', '/assets/presentation')
 
             # chown everything to tooloop user
-            self.chown_recursive(self.app_path, 'tooloop', 'tooloop')
+            self.chown_recursive(self.root_path, 'tooloop', 'tooloop')
             self.chown_recursive('/assets/data', 'tooloop', 'tooloop')
             self.chown_recursive('/assets/presentation', 'tooloop', 'tooloop')
 
